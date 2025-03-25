2b6d9ed05f1e328c1b8d599e5c65323972e8c423
Frank <frankwco@gmail.com>
2025-03-24 22:10:27 -0300

    aula_24_03_25

--- js/index.js
+++ js/index.js
@@ -1,15 +1,32 @@
-  function alterarTema(){
-            //DOM -> document object model
-            const tema = document.body.getAttribute("data-theme");
-            const novoTema = tema == 'dark' ? 'light' : 'dark';
-           /* let novoTema = '';
-            if(tema == 'dark'){
-                novoTema = 'light';
-            }else{
-                novoTema = 'dark';
-            }*/
-            document.body.setAttribute("data-theme", novoTema); 
-            
-            const btAlterarTema = document.getElementById("btAlterarTema");
-            btAlterarTema.textContent = novoTema == 'dark' ? 'Light' : 'Dark';
-        }
+document.addEventListener("DOMContentLoaded", (event)=>{
+    buscarInscritos();
+});
+
+function alterarTema() {
+    //DOM -> document object model
+    const tema = document.body.getAttribute("data-theme");
+    const novoTema = tema == 'dark' ? 'light' : 'dark';
+    /* let novoTema = '';
+     if(tema == 'dark'){
+         novoTema = 'light';
+     }else{
+         novoTema = 'dark';
+     }*/
+    document.body.setAttribute("data-theme", novoTema);
+
+    const btAlterarTema = document.getElementById("btAlterarTema");
+    btAlterarTema.textContent = novoTema == 'dark' ? 'Light' : 'Dark';
+}
+
+function buscarInscritos() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(res => {
+            const divInscritos = document.getElementById('inscritos');
+            res.forEach(user => {
+                const novoParagrafo = document.createElement("p");
+                novoParagrafo.textContent = `Nome: ${user.name}`;
+                divInscritos.appendChild(novoParagrafo);
+            });
+        }).catch(e => console.log(e));
+}
