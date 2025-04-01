2028f0a67ade9951493804a5720eb5a5606bae6c
Frank <frankwco@gmail.com>
2025-03-31 22:16:36 -0300

    aula_31_03_25

--- js/index.js
+++ js/index.js
@@ -1,6 +1,31 @@
-document.addEventListener("DOMContentLoaded", (event)=>{
+document.addEventListener("DOMContentLoaded", (event) => {
     buscarInscritos();
+    construirModal();
+
+    const temaLocal = localStorage.getItem("tema");
+    document.body.setAttribute("data-theme", temaLocal);
 });
+
+function construirModal() {
+    const botaoSaibaMais = document.getElementById("saiba-mais");
+    const modal = document.getElementById("modal");
+    const botaoFechar = document.getElementById("fechar-modal");
+
+    botaoFechar.addEventListener("click", ()=>{
+        modal.classList.add("hidden");
+    });
+
+    botaoSaibaMais.addEventListener("click", () => {
+        modal.classList.remove("hidden");
+    });
+
+    window.addEventListener("click", (e) => {
+        console.log(e.target);
+        if (e.target == modal) {
+            modal.classList.add("hidden");
+        }
+    });
+}
 
 function alterarTema() {
     //DOM -> document object model
@@ -12,6 +37,7 @@ function alterarTema() {
      }else{
          novoTema = 'dark';
      }*/
+    localStorage.setItem("tema", novoTema);
     document.body.setAttribute("data-theme", novoTema);
 
     const btAlterarTema = document.getElementById("btAlterarTema");
@@ -19,14 +45,18 @@ function alterarTema() {
 }
 
 function buscarInscritos() {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    //fetch("https://jsonplaceholder.typicode.com/users")
+    fetch("json/inscritos.json")
         .then(res => res.json())
         .then(res => {
+            //alert("Na requisição");
             const divInscritos = document.getElementById('inscritos');
             res.forEach(user => {
                 const novoParagrafo = document.createElement("p");
-                novoParagrafo.textContent = `Nome: ${user.name}`;
+                novoParagrafo.textContent = `Nome: ${user.nome}`;
                 divInscritos.appendChild(novoParagrafo);
             });
         }).catch(e => console.log(e));
+
+    //alert("Olá");
 }
